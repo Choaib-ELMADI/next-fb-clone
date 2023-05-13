d3b95e9a2e2411d4f320806b92ee7560dfc765fa
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-05-13 21:11:40 +0100

    Navbar is 90% done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,15 +4,92 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
 import { GoSearch } from 'react-icons/go';
+import { 
+    AiFillHome, AiOutlineHome,
+    AiFillVideoCamera, AiOutlineVideoCamera,
+    AiFillShop, AiOutlineShop,
+} from 'react-icons/ai';
+import { MdPeopleAlt, MdPeopleOutline } from 'react-icons/md'
+import { IoGameController, IoGameControllerOutline } from 'react-icons/io5';
+import { TbGridDots } from 'react-icons/tb';
+import { BsMessenger } from 'react-icons/bs';
+import { FaBell } from 'react-icons/fa';
 
 import './Navbar.scss';
 import images from '../../constants/images';
 import Searchbar from '../SearchBar/Searchbar';
 
+const links = [
+    {
+        name: 'home',
+        link: '/',
+        fill: <AiFillHome size={ 30 } />,
+        outline: <AiOutlineHome size={ 30 } />
+    },
+    {
+        name: 'friends',
+        link: '/',
+        fill: <MdPeopleAlt size={ 30 } />,
+        outline: <MdPeopleOutline size={ 30 } />
+    },
+    {
+        name: 'videos',
+        link: '/',
+        fill: <AiFillVideoCamera size={ 29 } />,
+        outline: <AiOutlineVideoCamera size={ 29 } />
+    },
+    {
+        name: 'shop',
+        link: '/',
+        fill: <AiFillShop size={ 30 } />,
+        outline: <AiOutlineShop size={ 30 } />
+    },
+    {
+        name: 'games',
+        link: '/',
+        fill: <IoGameController size={ 30 } />,
+        outline: <IoGameControllerOutline size={ 30 } />
+    },
+];
+
+const buttons = [
+    {
+        name: 'menu',
+        icon: <TbGridDots size={ 20 } />,
+    },
+    {
+        name: 'messenger',
+        icon: <BsMessenger size={ 20 } />,
+    },
+    {
+        name: 'notifications',
+        icon: <FaBell size={ 20 } />,
+    },
+    {
+        name: 'profile',
+        icon: <Image
+                className='image'
+                src={ images.choaib }
+                alt='Choaib ELMADI'
+                draggable={ false }
+        />,
+    },
+];
+
 
 
 const Navbar = () => {
     const [vueSearchList, setVueSearchList] = useState(false);
+    const [activeLink, setActiveLink] = useState('home');
+    const [clickedButton, setClickedButton] = useState(null);
+
+    const handleChoosedButton = (name) => {
+        if (clickedButton === name) {
+            setClickedButton(null)
+        } else {
+            setClickedButton(name);
+        }
+    };
 
     return (
         <nav className="navbar">
@@ -33,7 +110,39 @@ const Navbar = () => {
                 </div>
                 { vueSearchList && <Searchbar setVueSearchList={ setVueSearchList } /> }
             </div>
-            
+            <div className='main-section'>
+                {
+                    links.map((link, i) => (
+                        <Link 
+                            key={ `link-${ i }` }
+                            href={ link.link } 
+                            className={ activeLink === link.name ? 'link active' : 'link' }
+                            onClick={ () => setActiveLink(link.name) }
+                        >
+                            { 
+                                activeLink === link.name ?
+                                link.fill : 
+                                link.outline
+                            }
+                        </Link>
+                    ))
+                }
+            </div>
+            <div className='personal-section'>
+                {
+                    buttons.map((btn, i) => {
+                        return (
+                            <div 
+                                key={ `button-${ i }` }
+                                className={ clickedButton === btn.name ? 'active' : '' }
+                                onClick={ () => handleChoosedButton(btn.name) }
+                            >
+                                { btn.icon }
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </nav>
     );
 };
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,8 @@
 import facebook_logo from '../assets/facebook_2.png';
+import choaib        from '../assets/profile.jpg';
 
 const images = {
     facebook_logo,
+    choaib,
 };
 export default images;
